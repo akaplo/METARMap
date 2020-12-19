2b38988bc4ee074a87bc8c1cdb194593cc696d0e
Aaron Kaplowitz <akaplowitz@alum.cs.umass.edu>
2020-12-19 12:38:01 -0500

    Fix color

--- cycle_leds.py
+++ cycle_leds.py
@@ -8,7 +8,8 @@ LED_ORDER		= neopixel.RGB		# Strip type and colour ordering
 
 pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT)
 
-color = (255,0,0)
+color_red = (255,0,0)
+color_clear = (0,0,0)
 pixels[0] = color
 # turn them all on in groups of 10
 for multiplier in [1, 2, 3, 4, 5]:
@@ -16,13 +17,12 @@ for multiplier in [1, 2, 3, 4, 5]:
         p = 10 * multiplier - 10 + idx
         if (multiplier + idx != 1):
             print("turning on " + str(p))
-            pixels[p] = color
+            pixels[p] = color_red
             time.sleep(1)
     # now turn them all off
     for idxi in [0, 1, 2, 3,4,5,6,7,8]:
         if (multiplier + idxi != 1):
-            color = (0,0,0)
             p = 10 * multiplier - 10 + idxi
-            print("turning off " + str(p)
-            pixels[p] = color
+            print("turning off " + str(p))
+            pixels[p] = color_clear
 pixels.deinit()
