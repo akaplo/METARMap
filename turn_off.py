00509686569f802d89b33478f34dfb8da55ba556
Aaron Kaplowitz <akaplo@comcast.net>
2020-12-19 12:06:43 -0500

    Add extra script

--- turn_off.py
+++ turn_off.py
@@ -1,23 +1,20 @@
+
 import board
 import neopixel
 import time
 import os
-LED_COUNT		= 50			# Number of LED pixels.
-LED_PIN			= board.D18		# GPIO pin connected to the pixels (18 is PCM).
-LED_ORDER		= neopixel.RGB		# Strip type and colour ordering
+LED_COUNT               = 50                    # Number of LED pixels.
+LED_PIN                 = board.D18             # GPIO pin connected to the pixels (18 is PCM).
+LED_ORDER               = neopixel.RGB          # Strip type and colour ordering
 
 #script_dir = os.path.dirname(__file__)
 #airports_rel_path = 'airports'
 #airports_abs_path = os.path.join(script_dir, airports_rel_path)
 pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT)
 
-
-# turn them all on
-for idx in range(50):
-    color = (255,0,0)
-    pixels[idx] = color
-    time.sleep(1)
 # now turn them all off
 for idxi in range(50):
     color = (0,0,0)
     pixels[idxi] = color
+
+
