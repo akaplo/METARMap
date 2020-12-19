be8665a92dfef698797826127dac48bead2deacd
Aaron Kaplowitz <akaplowitz@alum.cs.umass.edu>
2020-12-19 12:16:00 -0500

    Cycle through in groups of 10

--- cycle_leds.py
+++ cycle_leds.py
@@ -6,18 +6,18 @@ LED_COUNT		= 50			# Number of LED pixels.
 LED_PIN			= board.D18		# GPIO pin connected to the pixels (18 is PCM).
 LED_ORDER		= neopixel.RGB		# Strip type and colour ordering
 
-#script_dir = os.path.dirname(__file__)
-#airports_rel_path = 'airports'
-#airports_abs_path = os.path.join(script_dir, airports_rel_path)
 pixels = neopixel.NeoPixel(LED_PIN, LED_COUNT)
 
-
-# turn them all on
-for idx in range(50):
-    color = (255,0,0)
-    pixels[idx] = color
-    time.sleep(1)
-# now turn them all off
-for idxi in range(50):
-    color = (0,0,0)
-    pixels[idxi] = color
+color = (255,0,0)
+pixels[0] = color
+# turn them all on in groups of 10
+for multiplier in [1, 2, 3, 4, 5]:
+    for idx in [0, 1, 2, 3,4,5,6,7,8]:
+        if multiplier is not 1 and idx is not 0:
+            pixels[10 * multiplier - 10 + idx] = color
+            time.sleep(1)
+    # now turn them all off
+    for idxi in [0, 1, 2, 3,4,5,6,7,8]::
+        if multiplier is not 1 and idx is not 0:
+            color = (0,0,0)
+            pixels[10 * multiplier - 10 + idxi] = color
